b2bad4db1c419cfc62a07fabdd2dc871b936abe1
TalissonMelo <talis.melo@petize.com.br>
2023-09-28 08:13:17 -0300

    Iniciando Projeto liberbox

---
+++ src/main/java/com/liberbox/LiberboxApplication.java
@@ -0,0 +1,13 @@
+package com.liberbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liberbox/LiberboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liberbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
